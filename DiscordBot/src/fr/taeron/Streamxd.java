3b4ae8608ec1e19a7c2a7a70cec3c125d7c90899
Taeron <trotibug@hotmail.fr>
2018-03-21 14:16:01 +0100

    gg

--- DiscordBot/src/fr/taeron/Streamxd.java
+++ DiscordBot/src/fr/taeron/Streamxd.java
@@ -2,5 +2,5 @@ package fr.taeron;
 
 public class Streamxd {
 
-	public static String token = "NDE0ODcxMDYzMzI4MTk0NTYw.DZL06Q.rZYuAfReSuu-bsRXLMZHC_GgWWo";
+	public static String token = "huh im fucking dumb";
 }
